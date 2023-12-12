a4301a234ef2fd026e55efee85821600ee4f2423
NATANI PETER <76612685+natani-peter@users.noreply.github.com>
2023-12-12 10:12:21 +0300

    Update views.py

--- users/views.py
+++ users/views.py
@@ -2,12 +2,13 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 from django.urls import reverse
 
-
+@csrf_exempt
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(data=request.POST)
